9b4a747da977f543fdaba8b374a97d68877c4e70
federicoalaniz <32943076+federicoalaniz@users.noreply.github.com>
2018-06-09 20:11:17 -0300

    Add files via upload

--- script/forms.js
+++ script/forms.js
@@ -1,3 +1,4 @@
+
 function validaContacto(){
     var regexFecha = /^(?:(?:(?:0?[1-9]|1\d|2[0-8])[/](?:0?[1-9]|1[0-2])|(?:29|30)[/](?:0?[13-9]|1[0-2])|31[/](?:0?[13578]|1[02]))[/](?:0{2,3}[1-9]|0{1,2}[1-9]\d|0?[1-9]\d{2}|[1-9]\d{3})|29[/]0?2[/](?:\d{1,2}(?:0[48]|[2468][048]|[13579][26])|(?:0?[48]|[13579][26]|[2468][048])00))$/;
     
@@ -23,12 +24,13 @@ function validaContacto(){
     else if(!regexCorreo.test(correo.value)){
         alert("Debe ingresar un correo válido");
         correo.focus();
+        return false;
     }
-    else if(!asunto.value)[
+    else if(!asunto.value){
         alert("Escriba un asunto");
         asunto.focus();
         return false;
-    ]
+    }    
     else if(!msg.value){
         alert("Debe escribir un mensaje");
         msg.focus();
@@ -39,5 +41,46 @@ function validaContacto(){
     }
 }
 function validaEncuesta(){
+    var nombre = document.getElementsByName('nombre');
+    var apellido = document.getElementsByName('apellido');
+    var email = document.getElementsByName('email');
+    var genero = document.getElementsByName('genero');
+    var forma = document.getElementsByName('forma');
+    var descripcion = document.getElementsByName('descripcion');
+    var regexCorreo = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
     
+    if(!nombre[0].value){
+        alert("Debe ingresar el nombre");
+        nombre[0].focus();
+        return false;
+    }
+    else if(!apellido[0].value){
+        alert("Debe ingresar su apellido");
+        apellido[0].focus();
+        return false;
+    }
+    else if(!regexCorreo.test(email[0].value)){
+        alert("Debe ingresar un correo válido");
+        email[0].focus();
+        return false;
+    }
+    else if(genero[0].selectedIndex == -1){
+        alert("Debe seleccionar el género");
+        genero[0].focus;
+        return false;
+    }
+    else if(forma[0].selectedIndex == -1){
+        alert("Debe seleccionar una forma de práctica");
+        forma[0].focus();
+        return false;
+    }
+    else if(!descripcion[0].value){
+        alert("Debe describir sus gustos y objetivos brevemente");
+        descripcion[0].focus();
+        return false;
+    }
+    else{
+        alert("Muchas gracias por su colaboración.\nLa encuesta fue enviada correctamente");
+        return true;
+    }
 }
---
+++ script/jquery.js
@@ -0,0 +1,5 @@
+$(document).ready(function(){
+    $("#btn-show-encuesta").click(function(){
+        $("#encuesta").show();
+    })
+})
